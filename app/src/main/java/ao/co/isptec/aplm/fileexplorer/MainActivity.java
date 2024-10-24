68dbfd4914497123be5b91c875f2c50ad11bb32f
Emanuel Pacavira <emanuelpacavira.09@gmail.com>
2024-10-24 23:24:18 +0100

    First commit

---
+++ app/src/main/java/ao/co/isptec/aplm/fileexplorer/MainActivity.java
@@ -0,0 +1,38 @@
+package ao.co.isptec.aplm.fileexplorer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button internalStorage;
+    private Button externalStorage;
+
+    @Override
+    public void onCreate(final Bundle icicle) {
+        super.onCreate(icicle);
+        this.setContentView(R.layout.activity_main);
+
+        this.internalStorage = (Button) findViewById(R.id.main_internal_storage_button);
+        this.internalStorage.setOnClickListener((View.OnClickListener) this);
+
+        this.externalStorage = (Button) findViewById(R.id.main_external_storage_button);
+        this.externalStorage.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void onClick(View v) {
+        if (v.equals(internalStorage)) {
+            startActivity(new Intent(this, InternalStorage.class));
+        } else if (v.equals(externalStorage)) {
+            startActivity(new Intent(this, ExternalStorage.class));
+        }
+    }
+}
